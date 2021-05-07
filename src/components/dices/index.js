ab9bca0da2810160168ecea6a0c0478694d2c4b6
Israel Castro <israel.castro@gmail.com>
2021-05-07 16:11:50 -0500

    [Done] dices and results on click

--- src/components/dices/index.js
+++ src/components/dices/index.js
@@ -3,11 +3,20 @@ import React, { useState, useRef } from 'react';
 import './index.css';
 
 function Dices() {
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState('Click this button below to start');
 
   const user1 = useRef(null);
   const user2 = useRef(null);
   const resultDice = useRef(null);
+
+  let diceImages = [
+    '../images/dice1.svg',
+    '../images/dice2.svg',
+    '../images/dice3.svg',
+    '../images/dice4.svg',
+    '../images/dice5.svg',
+    '../images/dice6.svg',
+  ];
 
   const roll = () => {
     const firstRandomNum = Math.floor(Math.random() * 6);
@@ -23,10 +32,10 @@ function Dices() {
       // console.log(`It's a draw!`);
       setResult(`It's a draw!`);
     }
-    console.log(firstRandomNum, secondRandomNum);
+    console.log(firstRandomNum + 1, secondRandomNum + 1);
 
-    user1.current.setAttribute('src', '/images/dice1.svg');
-    user2.current.setAttribute('src', '/images/dice2.svg');
+    user1.current.setAttribute('src', diceImages[firstRandomNum]);
+    user2.current.setAttribute('src', diceImages[secondRandomNum]);
   };
 
   return (
@@ -34,11 +43,11 @@ function Dices() {
       <div className='dice-wrapper'>
         <div className='dice-area'>
           <p>User 1</p>
-          <img ref={user1} />
+          <img src='../images/dice1.svg' ref={user1} alt='Dice' />
         </div>
         <div className='dice-area'>
           <p>User 2</p>
-          <img ref={user2} />
+          <img src='../images/dice1.svg' ref={user2} alt='Dice' />
         </div>
       </div>
       <p className='result' ref={resultDice}>
