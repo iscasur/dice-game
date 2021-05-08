08fe9927554e6ccab7cc667026abee863c163a64
Israel Castro <israel.castro@gmail.com>
2021-05-08 15:38:35 -0500

    [Add] results history

--- src/components/dices/index.js
+++ src/components/dices/index.js
@@ -10,38 +10,56 @@ import './index.css';
 
 function Dices() {
   const [result, setResult] = useState('Click this button below to start');
+  const [history, setHistory] = useState([]);
 
   const user1 = useRef(null);
   const user2 = useRef(null);
   const resultDice = useRef(null);
+  const titleHistoryResult = useRef(null);
 
-  let diceImages = [
-    dice1,
-    dice2,
-    dice3,
-    dice4,
-    dice5,
-    dice6,
-  ];
+  let diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
 
   const roll = () => {
+    // Generate random number
     const firstRandomNum = Math.floor(Math.random() * 6);
     const secondRandomNum = Math.floor(Math.random() * 6);
 
+    // Logic for this game
     if (firstRandomNum > secondRandomNum) {
-      // console.log(`The winner is user 1 with ${firstRandomNum + 1} points`);
       setResult(`The winner is user 1 with ${firstRandomNum + 1} points`);
+      setHistory([
+        ...history,
+        {
+          id: history.length,
+          value: `User 1 won with ${firstRandomNum + 1} points`,
+        },
+      ]);
     } else if (firstRandomNum < secondRandomNum) {
-      // console.log(`The winner is user 2 with ${secondRandomNum + 1} points`);
       setResult(`The winner is user 2 with ${secondRandomNum + 1} points`);
+      setHistory([
+        ...history,
+        {
+          id: history.length,
+          value: `User 2 won with ${firstRandomNum + 1} points`,
+        },
+      ]);
     } else {
-      // console.log(`It's a draw!`);
       setResult(`It's a draw!`);
+      setHistory([
+        ...history,
+        {
+          id: history.length,
+          value: `It was a draw! Both user got ${firstRandomNum + 1} points`,
+        },
+      ]);
     }
-    console.log(firstRandomNum + 1, secondRandomNum + 1);
 
     user1.current.setAttribute('src', diceImages[firstRandomNum]);
     user2.current.setAttribute('src', diceImages[secondRandomNum]);
+  };
+
+  const reset = () => {
+    setHistory([]);
   };
 
   return (
@@ -59,7 +77,18 @@ function Dices() {
       <p className='result' ref={resultDice}>
         {result}
       </p>
-      <button onClick={roll}>Roll the dice</button>
+      <button onClick={roll} className='btn'>
+        Roll the dice
+      </button>
+      <button onClick={reset} className='btn reset'>
+        Reset
+      </button>
+      <h2 ref={titleHistoryResult}>History</h2>
+      <ul>
+        {history.map((item) => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
     </>
   );
 }
